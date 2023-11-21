f3ec42603869bc49011a8e2443916ae98f9a7984
terujd <jansson.michael.93@gmail.com>
2023-11-21 21:19:28 +0200

    added apiUrls

--- angul-it/src/environments/environment.prod.ts
+++ angul-it/src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
-export const environment = {// Environment object
-  recaptcha: {// Recaptcha object
-      siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'// Site key for the Recaptcha
-    },
-    production: false// Production flag
-  };
+export const environment = {
+  production: true,
+  recaptcha: {
+    siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'
+  },
+  apiUrl: 'https://terujd.github.io/angul-it/'
+};
+
+
+// export const environment = {// Environment object
+//   recaptcha: {// Recaptcha object
+//       siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'// Site key for the Recaptcha
+//     },
+//     production: false// Production flag
+//   };
--- angul-it/src/environments/environment.ts
+++ angul-it/src/environments/environment.ts
@@ -1,6 +1,15 @@
-export const environment = {// Environment object
-recaptcha: {// Recaptcha object
-    siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'// Site key for the Recaptcha
+export const environment = {
+  production: false,
+  recaptcha: {
+    siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'
   },
-  production: false // Production flag
+  apiUrl: 'http://localhost:4200/'
 };
+
+
+// export const environment = {// Environment object
+// recaptcha: {// Recaptcha object
+//     siteKey: '6Lf5eOkoAAAAAAuKH5i9UNeaDLOaSdnUuqe9iKcT'// Site key for the Recaptcha
+//   },
+//   production: false // Production flag
+// };
